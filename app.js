cd4f6b72e5585b1e1dc9a2dbd10f40c3a6f641dd
Broc Atkinson <44753400+BrocAtkinson@users.noreply.github.com>
2019-08-19 20:53:38 -0600

    Add files via upload

--- app.js
+++ app.js
@@ -4,18 +4,42 @@
  //   img.style.visibility = (visible ? 'visible' : 'hidden');
 
 
-$(function () {
-    $("#about").on("cick",function () {
-        $("#about").removeClass("bg1");
-        console.log("about");
-        $(".container_about p").text("I am currently a full time certified paralegal, hoping this course will help me break into the web development field");
-    });
-});
+
+
+function about(){
+var about = document.getElementById("about");
+if (about.style.display === "none"){
+    about.style.display = "block";
+} else{
+    about.style = "none";
+}
+}
+
+
+
+
+//  $(function () {
+//     $("#about").on("cick",function () {
+//         $("#about").removeClass("bg1");
+//         console.log("about");
+//         $(".container_about p").text("I am currently a full time certified paralegal, hoping this course will help me break into the web development field");
+//     });
+// });
+
+// function myFunction() {
+//     var x = document.getElementById("myDIV");
+//     if (x.style.display === "none") {
+//       x.style.display = "block";
+//     } else {
+//       x.style.display = "none";
+//     }
+//   }
+  
 $(function () {
     $("#profile").on("click",function () {
         $("#profile").addClass("bg2");
         console.log(click);
-        $("p").html("The link below is from my bootcamps 1st group project, where I handeled the API call for the New York Times and the quick seach buttons that populate the top 5 coins.");
+        $("container_profile").html("The link below is from my bootcamps 1st group project, where I handeled the API call for the New York Times and the quick seach buttons that populate the top 5 coins.");
         
     });
 });
